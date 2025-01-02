c2bb1d897af37213c0b35c7e4b3510de5d57996a
sintayehua <sentaddis@gmail.com>
2025-01-02 16:55:23 +0300

    expander removed

--- pages/7_⛰️_CMW.py
+++ pages/7_⛰️_CMW.py
@@ -14,39 +14,39 @@ st.sidebar.info(markdown)
 logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
-st.title("Split-panel Map")
+st.title("Choke Mountain Watersheds")
 
-with st.expander("See source code"):
-    with st.echo():
-        m = leafmap.Map()
-        boundary = "https://raw.githubusercontent.com/sintayehua/streamlit-maps/main/data/cmw_max_boundary.geojson"
-        climate = "https://raw.githubusercontent.com/sintayehua/streamlit-maps/main/data/nma_stations.csv"
-        flow = "https://raw.githubusercontent.com/sintayehua/streamlit-maps/main/data/flow_stations.csv"
-        landcover = "https://raw.githubusercontent.com/sintayehua/streamlit-maps/main/data/landcover.tif"
-        m.add_geojson(boundary, layer_name="CMW boundary")
-        m.add_points_from_xy(
-            climate,
-            x="long",
-            y="lat",
-            #color_column="station_name",
-            #icon_names=["gear", "map", "leaf", "globe"],
-            spin=True,
-            add_legend=True,
-        )
-        m.add_points_from_xy(
-            flow,
-            x="lon",
-            y="lat",
-            #color_column="station_name",
-            #icon_names=["gear", "map", "leaf", "globe"],
-            spin=False,
-            add_legend=True,
-        )
-        m.add_raster(landcover, colormap="terrain", layer_name="Landcover")
-        #m.add_legend()
-        #m.split_map(
-        #    left_layer="ESA WorldCover 2020 S2 FCC", right_layer="ESA WorldCover 2020"
-        #)
-        #m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
-
+#with st.expander("See source code"):
+with st.echo():
+    m = leafmap.Map()
+    boundary = "https://raw.githubusercontent.com/sintayehua/streamlit-maps/main/data/cmw_max_boundary.geojson"
+    climate = "https://raw.githubusercontent.com/sintayehua/streamlit-maps/main/data/nma_stations.csv"
+    flow = "https://raw.githubusercontent.com/sintayehua/streamlit-maps/main/data/flow_stations.csv"
+    landcover = "https://raw.githubusercontent.com/sintayehua/streamlit-maps/main/data/landcover.tif"
+    m.add_geojson(boundary, layer_name="CMW boundary")
+    m.add_points_from_xy(
+        climate,
+        x="long",
+        y="lat",
+        #color_column="station_name",
+        #icon_names=["gear", "map", "leaf", "globe"],
+        spin=True,
+        add_legend=True,
+    )
+    m.add_points_from_xy(
+        flow,
+        x="lon",
+        y="lat",
+        #color_column="station_name",
+        #icon_names=["gear", "map", "leaf", "globe"],
+        spin=False,
+        add_legend=True,
+    )
+    m.add_raster(landcover, colormap="terrain", layer_name="Landcover")
+    #m.add_legend()
+    #m.split_map(
+    #    left_layer="ESA WorldCover 2020 S2 FCC", right_layer="ESA WorldCover 2020"
+    #)
+    #m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
+    
 m.to_streamlit(height=700)
